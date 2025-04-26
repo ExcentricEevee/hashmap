d4d3cb592bcd6fbc2bc2818a85af0d5918e5e890
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-04-26 17:43:09 -0400

    Implement a way to hash string values
    This method was directly taken from The Odin Project's project page for
    HashMap. It is designed with the idea that it should only be accepting
    string values as a key.

--- hashmap.rb
+++ hashmap.rb
@@ -4,4 +4,13 @@ class HashMap
     @load_factor = 0.8
     @capacity = 16
   end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
 end

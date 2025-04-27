68663ab3ddb836b169c3d87c12028336e2233ee2
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-04-26 20:05:12 -0400

    Add #set method for filling buckets with Nodes
    This method is perfectly capable of either filling an empty bucket with
    a Node or updating the value of an existing Node if the keys match.
    However, it does not yet deal with collisions. For now all that happens
    in the event of a collision is nothing; the operation does not go
    through.

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,15 +1,34 @@
+require_relative "node"
+
 # HashMap data structure for matching key/value pairs
 class HashMap
+  attr_reader :buckets
+
   def initialize
     @load_factor = 0.8
     @capacity = 16
     @buckets = Array.new(capacity)
   end
 
+  def set(key, value)
+    index = hash(key) % capacity
+    # Prevent Ruby from dynamically resizing the buckets array
+    raise IndexError if index.negative? || index >= @buckets.length
+
+    if buckets[index].nil?
+      buckets[index] = Node.new(key, value)
+    elsif buckets[index].key == key
+      buckets[index].value = value
+    #else
+      # Collision: deal with it by making a linked list
+      #buckets[index].next_node = Node.new(key, value)
+    end
+  end
+
   private
 
   attr_reader :load_factor
-  attr_accessor :capacity, :buckets
+  attr_accessor :capacity
 
   def hash(key)
     hash_code = 0
